3a12b8037c843ddad9ba26b9f855949b49c51481
tui.ono <tui.ono@mujin.co.jp>
2023-04-27 11:53:45 +0900

    Added missing argument for manual placement recovery

--- python/mujinplanningclient/binpickingplanningclient.py
+++ python/mujinplanningclient/binpickingplanningclient.py
@@ -576,7 +576,7 @@ class BinpickingPlanningClient(realtimerobotplanningclient.RealtimeRobotPlanning
         taskparameters.update(kwargs)
         return self.ExecuteCommand(taskparameters, timeout=timeout, fireandforget=fireandforget)
 
-    def ManuallyPlacePackItem(self, packFormationComputationResult=None, inputPartIndex=None, placeLocationNames=None, placedTargetPrefix=None, dynamicGoalsGeneratorParameters=None, orderNumber=None, numLeftToPick=None, timeout=10, fireandforget=False):
+    def ManuallyPlacePackItem(self, packFormationComputationResult=None, inputPartIndex=None, placeLocationNames=None, placedTargetPrefix=None, dynamicGoalsGeneratorParameters=None, intMaxPackageTranslationOffsetXY=None, orderNumber=None, numLeftToPick=None, timeout=10, fireandforget=False):
         """
         Places an item according to the pack formation assuming the item is placed manually and updates robotbridge state
         """
@@ -596,6 +596,8 @@ class BinpickingPlanningClient(realtimerobotplanningclient.RealtimeRobotPlanning
             taskparameters['placedTargetPrefix'] = placedTargetPrefix
         if dynamicGoalsGeneratorParameters is not None:
             taskparameters['dynamicGoalsGeneratorParameters'] = dynamicGoalsGeneratorParameters
+        if intMaxPackageTranslationOffsetXY is not None:
+            taskparameters['intMaxPackageTranslationOffsetXY'] = intMaxPackageTranslationOffsetXY
         return self.ExecuteCommand(taskparameters, timeout=timeout, fireandforget=fireandforget)
 
     def SendPackFormationComputationResult(self, timeout=10, fireandforget=False, **kwargs):
