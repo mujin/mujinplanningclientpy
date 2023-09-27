5d76b683017379d92b65373f69d77696a6af4b93
heman.gandhi <heman.gandhi@mujin.co.jp>
2023-09-27 10:24:15 +0900

    Use the RealtimeRobotPlanningClient's robotname in the HandEyeCalibrationClient

--- python/mujinplanningclient/handeyecalibrationplanningclient.py
+++ python/mujinplanningclient/handeyecalibrationplanningclient.py
@@ -15,15 +15,13 @@ class HandEyeCalibrationPlanningClient(RealtimeRobotPlanningClient):
 
     tasktype = 'handeyecalibration'
 
-    robot = None
-
     _deprecated = None # used to mark arguments as deprecated (set argument default value to this)
 
-    def __init__(self, robot, **kwargs):
+    def __init__(self, **kwargs):
         """Connects to the Mujin controller, initializes HandEyeCalibration task and sets up parameters
 
         Args:
-            robot (str): Name of the robot, e.g. VP-5243I
+            robotname (str): Name of the robot, e.g. VP-5243I
             taskzmqport (int, optional): Port of the task's ZMQ server, e.g. 7110. (Default: 11000)
             taskheartbeatport (int, optional): Port of the task's ZMQ server's heartbeat publisher, e.g. 7111. (Default: 11001)
             taskheartbeattimeout (float, optional): Seconds until reinitializing task's ZMQ server if no heartbeat is received, e.g. 7
@@ -39,7 +37,6 @@ class HandEyeCalibrationPlanningClient(RealtimeRobotPlanningClient):
             ignoredArgs: Additional keyword args are not used, but allowed for easy initialization from a dictionary
         """
         super(HandEyeCalibrationPlanningClient, self).__init__(tasktype=self.tasktype, **kwargs)
-        self.robot = robot
 
 
     #
@@ -78,8 +75,10 @@ class HandEyeCalibrationPlanningClient(RealtimeRobotPlanningClient):
             taskparameters['calibboardGeomName'] = calibboardGeomName
         if gridindex is not None:
             taskparameters['gridindex'] = gridindex
-        if self.robot is not None:
-            taskparameters['robot'] = self.robot
+        if 'robot' in kwargs:
+            taskparameters['robot'] = kwargs['robot']
+        elif self._robotname is not None:
+            taskparameters['robot'] = self._robotname
         taskparameters.update(kwargs)
         return self.ExecuteCommand(taskparameters, timeout=timeout)
 
@@ -121,8 +120,10 @@ class HandEyeCalibrationPlanningClient(RealtimeRobotPlanningClient):
             taskparameters['minPatternTiltAngle'] = minPatternTiltAngle
         if maxPatternTiltAngle is not None:
             taskparameters['maxPatternTiltAngle'] = maxPatternTiltAngle
-        if self.robot is not None:
-            taskparameters['robot'] = self.robot
+        if 'robot' in kwargs:
+            taskparameters['robot'] = kwargs['robot']
+        elif self._robotname is not None:
+            taskparameters['robot'] = self._robotname
         taskparameters.update(kwargs)
         return self.ExecuteCommand(taskparameters, timeout=timeout)
 
