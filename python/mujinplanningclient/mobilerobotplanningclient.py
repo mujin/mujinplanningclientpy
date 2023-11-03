e330543f496cde004650fa701cb781deb2d05775
Ross Schlaikjer <ross.schlaikjer@mujin.co.jp>
2023-11-03 11:16:55 +0900

    Add StartPlanningThread

--- python/mujinplanningclient/mobilerobotplanningclient.py
+++ python/mujinplanningclient/mobilerobotplanningclient.py
@@ -16,6 +16,32 @@ class MobileRobotPlanningClient(planningclient.PlanningClient):
         """Connects to the Mujin controller, initializes task"""
         super(MobileRobotPlanningClient, self).__init__(tasktype=self.TASK_TYPE, **kwargs)
 
+    def StartPlanningThread(self, robotBridgeConnectionInfo=None, chargingParameters=None):
+        """
+        Initialize the planning thread.
+        Robot bridge connection info may be specified as a dictionary with the following fields:
+            use: bool: Whether the planning task should connect to robot bridges. Defaults to false.
+            host: string: Hostname of the robot bridge server to communicate with
+            port: string: Port of the robot bridge server to communicate with
+            queueid: string: Name of the queue ID to use. Defaults to the slave request id.
+        Charging parameters may be specified as a dictionary with the following fields:
+            minimumChargeTimeS: int: A robot that begins charging will always charge for at least this duration
+            lowPowerThresholdPercent: Battery percentage [0, 100) below which a robot will be considered for charging
+            sufficientPowerThresholdPercent: Battery percentage [0, 100) above which a robot will yield a charger to another robot
+            highPowerThresholdPercent: Battery percentage [0, 100) above which a robot will disengage from the charger
+        """
+        # Some of the expected parameters here do not match the standard naming convention.
+        # Check that no accidentally misnamed parameters exist.
+        if robotBridgeConnectionInfo:
+            assert 'queueId' not in robotBridgeConnectionInfo
+
+        taskparameters = {
+            'command': 'StartPlanningThread',
+            'robotBridgeConnectionInfo': robotBridgeConnectionInfo,
+            'chargingParameters': chargingParameters,
+        }
+        return self.ExecuteCommand(taskparameters)
+
     def GetTaskGraph(self):
         """Get the current state of the task graph"""
         taskparameters = {
