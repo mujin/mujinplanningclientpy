df97cc63bd641a7095a3e94b4ba04f327a3e9afb
justin.schaare <justin.schaare@mujin.co.jp>
2023-11-30 10:10:00 +0900

    fix realtime itl client init, update types

--- python/mujinplanningclient/handeyecalibrationplanningclient.py
+++ python/mujinplanningclient/handeyecalibrationplanningclient.py
@@ -42,7 +42,7 @@ class HandEyeCalibrationPlanningClient(RealtimeRobotPlanningClient):
         callerid='',
         **ignoredArgs
     ):
-        # type: (str, Optional[float], Optional[float], float, Optional[str], int, int, float, Optional[zmq.Context], Optional[str], str, str, str, str, str, str, Any) -> None
+        # type: (Optional[str], Optional[float], Optional[float], float, Optional[str], int, int, float, Optional[zmq.Context], Optional[str], str, str, str, str, str, str, Any) -> None
         """Connects to the Mujin controller, initializes HandEyeCalibration task and sets up parameters
 
         Args:
